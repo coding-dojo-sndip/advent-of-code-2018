f8ea02f83baea085c00866d470b0ba8b88dbfc31
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-01-08 14:16:34 +0100

    Day01, part1

--- src/main/java/fr/insee/aoc/Day01.java
+++ src/main/java/fr/insee/aoc/Day01.java
@@ -1,5 +1,13 @@
 package fr.insee.aoc;
 
 public class Day01 implements Day {
+
+	@Override
+	public String part1(String input) {
+		int frequency = DayUtils.readLines(input).stream()
+			.mapToInt(Integer::parseInt)
+			.sum();
+		return String.valueOf(frequency);
+	}
 	
 }
