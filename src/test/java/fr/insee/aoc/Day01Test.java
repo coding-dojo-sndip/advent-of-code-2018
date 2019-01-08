292a240de1a80216cb2dc7d303f6e604b570b04e
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-01-08 14:14:25 +0100

    Squelette exercice 1

--- src/test/java/fr/insee/aoc/Day01Test.java
+++ src/test/java/fr/insee/aoc/Day01Test.java
@@ -24,15 +24,14 @@ public class Day01Test  {
 	
 	@Test
 	public void part1() {
-		String answer = day.part1("src/test/resources/01.txt");
+		String answer = day.part1("src/main/resources/01.txt");
 		System.out.println(String.format("%s.1: %s", day.getClass().getSimpleName(), answer));
 		Assert.assertEquals("", answer);
 	}
 	
-	
 	@Test
 	public void part2() {
-		String answer = day.part2("src/test/resources/01.txt");
+		String answer = day.part2("src/main/resources/01.txt");
 		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
 		Assert.assertEquals("", day.part2("src/test/resources/01-3.txt"));
 	}
