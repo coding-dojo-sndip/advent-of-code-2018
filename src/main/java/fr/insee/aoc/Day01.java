2ba0660996d356b615591c1756ca6523260e2cfc
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-01-10 14:03:04 +0100

    refactor exercice 1 partie 1

--- src/main/java/fr/insee/aoc/Day01.java
+++ src/main/java/fr/insee/aoc/Day01.java
@@ -1,11 +1,15 @@
 package fr.insee.aoc;
 
-import fr.insee.aoc.utils.DayUtils;
+import static fr.insee.aoc.utils.DayUtils.streamLines;
 
 public class Day01 implements Day {
 
 	public String part1(String input) {
-		int sum = DayUtils.readLines(input).stream().mapToInt(Integer::valueOf).sum();
+		int sum = streamLines(input).mapToInt(Integer::valueOf).sum();
 		return String.valueOf(sum);
+	}
+	
+	public String part2(String input) {
+		return null;
 	}
 }
--- src/main/java/fr/insee/aoc/utils/DayUtils.java
+++ src/main/java/fr/insee/aoc/utils/DayUtils.java
@@ -2,7 +2,10 @@ package fr.insee.aoc.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
 
@@ -13,6 +16,15 @@ public class DayUtils {
 	public static List<String> readLines(String input) {
 		try {
 			return FileUtils.readLines(new File(input), "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		throw new DayException("Erreur dans la lecture du fichier");
+	}
+	
+	public static Stream<String> streamLines(String input){
+		try {
+			return Files.lines(Paths.get(input));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
