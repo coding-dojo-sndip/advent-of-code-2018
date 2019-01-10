e9097a3d6149a0788cf2ccb39d4a11d5331b069c
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-01-10 09:01:06 +0100

    Mise en place Day02

---
+++ src/test/java/fr/insee/aoc/Day02Test.java
@@ -0,0 +1,28 @@
+package fr.insee.aoc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day02Test  {
+    
+	private Day day = new Day02();
+	
+	@Test
+	public void case0() {
+		Assert.assertEquals("12", day.part1("src/test/resources/02-0.txt"));
+	}
+	
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/02.txt");
+		System.out.println(String.format("%s.1: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("", answer);
+	}
+	
+	@Test
+	public void part2() {
+		String answer = day.part2("src/main/resources/02.txt");
+		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("", answer);
+	}
+}
