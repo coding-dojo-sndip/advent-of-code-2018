66a7053a65667cc8a3f6bc0a01c6acfe646b97ba
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-01-17 13:40:34 +0100

    suppression des dépendances Maven (fixes #1)

--- src/main/java/fr/insee/aoc/Day.java
+++ src/main/java/fr/insee/aoc/Day.java
@@ -1,14 +1,12 @@
 package fr.insee.aoc;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 public interface Day {
 
 	default String part1(String input) {
-		throw new NotImplementedException("Not implemented yet!");
+		throw new DayException("Not implemented yet!");
 	}
 	
 	default String part2(String input) {
-		throw new NotImplementedException("Not implemented yet!");
+		throw new DayException("Not implemented yet!");
 	}
 }
--- src/main/java/fr/insee/aoc/Days.java
+++ src/main/java/fr/insee/aoc/Days.java
@@ -1,26 +1,24 @@
 package fr.insee.aoc;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.apache.commons.io.FileUtils;
-
 public class Days {
-	
+
 	public static List<String> readLines(String input) {
 		try {
-			return FileUtils.readLines(new File(input), "UTF-8");
+			return Files.readAllLines(Paths.get(input), Charset.forName("UTF-8"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		throw new DayException("Erreur dans la lecture du fichier");
 	}
-	
-	public static Stream<String> streamLines(String input){
+
+	public static Stream<String> streamLines(String input) {
 		try {
 			return Files.lines(Paths.get(input));
 		} catch (IOException e) {
