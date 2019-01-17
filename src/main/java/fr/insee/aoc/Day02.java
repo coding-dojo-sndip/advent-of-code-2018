9381218dcba592963fbf2461cf6b143fa246cbdf
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-01-17 14:32:38 +0100

    day2 exercice1

--- src/main/java/fr/insee/aoc/Day02.java
+++ src/main/java/fr/insee/aoc/Day02.java
@@ -1,5 +1,19 @@
 package fr.insee.aoc;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Day02 implements Day {
+
+	@Override
+	public String part1(String input) {
+		// TODO Auto-generated method stub
+		return Day.super.part1(input);
+	}
+	
+	private Map<Integer, Long>occurences(String string){
+		return string.chars().boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	}
 	
 }
