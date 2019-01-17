49da850a4d7494a837ddbae0711ae390d1d6d74d
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-01-17 14:44:50 +0100

    day2 exercice1 suite

--- src/main/java/fr/insee/aoc/Day02.java
+++ src/main/java/fr/insee/aoc/Day02.java
@@ -1,19 +1,27 @@
 package fr.insee.aoc;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import static fr.insee.aoc.Days.*;
 
 public class Day02 implements Day {
 
 	@Override
 	public String part1(String input) {
-		// TODO Auto-generated method stub
-		return Day.super.part1(input);
+		List<String>strings = listOfLines(input);
+		long a = strings.stream().map(this::occurences).filter(o -> containsSameCharacter(o, 2)).count();
+		long b = strings.stream().map(this::occurences).filter(o -> containsSameCharacter(o, 3)).count();
+		return String.valueOf(a*b);
 	}
 	
 	private Map<Integer, Long>occurences(String string){
 		return string.chars().boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	}
+	
+	private boolean containsSameCharacter(Map<Integer, Long> occurences, int numberOfOccurences) {
+		return occurences.values().contains(Long.valueOf(numberOfOccurences));
 	}
 	
 }
