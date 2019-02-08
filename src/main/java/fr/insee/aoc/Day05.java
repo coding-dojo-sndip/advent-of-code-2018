2865e232f7bf19dbfa1a9f9d4e39d7e57b7790e9
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-02-08 09:55:08 +0100

    Mise en place jour 5

---
+++ src/main/java/fr/insee/aoc/Day05.java
@@ -0,0 +1,7 @@
+package fr.insee.aoc;
+
+import static fr.insee.aoc.Days.*;
+
+public class Day05 implements Day {
+
+}
---
+++ src/test/java/fr/insee/aoc/Day05Test.java
@@ -0,0 +1,33 @@
+package fr.insee.aoc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day05Test {
+    
+	private Day day = new Day05();
+	
+	@Test
+	public void case1_0() {
+		Assert.assertEquals("10", day.part1("src/test/resources/05-0.txt"));
+	}
+	
+	@Test
+	public void case2_0() {
+		Assert.assertEquals("4", day.part2("src/test/resources/05-0.txt"));
+	}
+	
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/05.txt");
+		System.out.println(String.format("%s.1: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("", answer);
+	}
+	
+	@Test
+	public void part2() {
+		String answer = day.part2("src/main/resources/05.txt");
+		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("", answer);
+	}
+}
