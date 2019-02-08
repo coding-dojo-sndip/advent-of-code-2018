fbd77af42a4ce97346cdac79d0a09b8a03da57b1
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-02-08 13:03:58 +0100

    Day05 part1, rapide

--- src/main/java/fr/insee/aoc/Day05.java
+++ src/main/java/fr/insee/aoc/Day05.java
@@ -4,4 +4,46 @@ import static fr.insee.aoc.Days.*;
 
 public class Day05 implements Day {
 
+    @Override
+    public String part1(String input) {
+        Reaction reaction = new Reaction(readLine(input));
+        while(reaction.stillReacts()) {
+            reaction.react();
+        }
+        return String.valueOf(reaction.polymer.length());
+    }
+
+    static class Reaction {
+        StringBuilder polymer;
+        int index;
+
+        Reaction(String polymer) {
+            this.polymer = new StringBuilder(polymer);
+            this.index = 0;
+        }
+
+        boolean stillReacts() {
+            // System.out.println(index);
+            return index < polymer.length();
+        }
+
+        void react() {
+            react(index, index + 1);
+        }
+
+        private void react(int left, int right) {
+            if(left >=0 && right < polymer.length() && willReact(polymer.charAt(left), polymer.charAt(right))){
+                index --;
+                react( left - 1, right + 1);
+            }
+            else {
+                index ++;
+                polymer.delete(left + 1, right);
+            }
+        }
+
+        private boolean willReact(char a, char b) {
+            return Math.abs(a - b) == 32;
+        }
+    }
 }
