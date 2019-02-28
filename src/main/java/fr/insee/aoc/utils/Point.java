b800ef58108da59e29d33d90d2f0721b99989295
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-02-28 15:35:35 +0100

    Classe Point, fix #17

--- src/main/java/fr/insee/aoc/utils/Point.java
+++ src/main/java/fr/insee/aoc/utils/Point.java
@@ -1,17 +1,56 @@
 package fr.insee.aoc.utils;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 public class Point implements Comparable<Point> {
-	
+
+	private int x, y;
+
+	private static final Comparator<Point> comparator = Comparator.comparingInt(Point::getY)
+			.thenComparingInt(Point::getX);
+
+	private Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
 	public static Point of(int x, int y) {
-		throw new DayException("Not implemented yet!"); // TODO
+		return new Point(x, y);
 	}
 
 	@Override
 	public int compareTo(Point other) {
-		throw new DayException("Not implemented yet!"); // TODO
+		return comparator.compare(this, other);
 	}
 
 	public int manhattan(Point other) {
-		throw new DayException("Not implemented yet!"); // TODO
+		return Math.abs(this.x - other.x) + Math.abs(this.y - other.y);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.x, this.y);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object)
+			return true;
+		if (object == null)
+			return false;
+		if (object instanceof Point) {
+			Point other = (Point) object;
+			return Objects.equals(this.x, other.x) && Objects.equals(this.y, other.y);
+		}
+		return false;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
 	}
 }
