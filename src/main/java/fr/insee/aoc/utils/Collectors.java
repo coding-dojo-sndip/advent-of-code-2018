31a1187107c8bba73cfc8c89224083cafc96fe42
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-02-28 15:36:17 +0100

    Collectors listMin et listMax, fix #19

--- src/main/java/fr/insee/aoc/utils/Collectors.java
+++ src/main/java/fr/insee/aoc/utils/Collectors.java
@@ -1,6 +1,9 @@
 package fr.insee.aoc.utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
@@ -9,46 +12,82 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 
-public class Collectors<T> implements Collector<T, List<T>, List<T>>{
-	
+public class Collectors<T> implements Collector<T, List<T>, List<T>> {
+
+	private Comparator<? super T> comparator;
+
+	public Collectors(Comparator<? super T> comparator) {
+		this.comparator = comparator;
+	}
+
 	public static <T> Collectors<T> listOfMax(Comparator<? super T> comparator) {
-		throw new DayException("Not implemented yet!"); // TODO
+		return new Collectors<>(comparator.reversed());
 	}
-	
+
 	public static <T> Collectors<T> listOfMin(Comparator<? super T> comparator) {
-		throw new DayException("Not implemented yet!"); // TODO
+		return new Collectors<>(comparator);
 	}
-	
+
 	public static <T extends Comparable<T>> Collectors<T> listOfMax() {
-		throw new DayException("Not implemented yet!"); // TODO
+		return new Collectors<>(Comparator.reverseOrder());
 	}
-	
+
 	public static <T extends Comparable<T>> Collectors<T> listOfMin() {
-		throw new DayException("Not implemented yet!"); // TODO
+		return new Collectors<>(Comparator.naturalOrder());
 	}
 
 	@Override
 	public Supplier<List<T>> supplier() {
-		throw new DayException("Not implemented yet!"); // TODO
+		return ArrayList::new;
 	}
 
 	@Override
 	public BiConsumer<List<T>, T> accumulator() {
-		throw new DayException("Not implemented yet!"); // TODO
+		return new BiConsumer<List<T>, T>() {
+
+			@Override
+			public void accept(List<T> list, T t) {
+				if (list.isEmpty() || comparator.compare(list.get(0), t) == 0) {
+					list.add(t);
+				} else if (comparator.compare(list.get(0), t) > 0) {
+					list.clear();
+					list.add(t);
+				}
+			}
+		};
 	}
 
 	@Override
 	public BinaryOperator<List<T>> combiner() {
-		throw new DayException("Not implemented yet!"); // TODO
+		return new BinaryOperator<List<T>>() {
+
+			@Override
+			public List<T> apply(List<T> a, List<T> b) {
+				if (a.isEmpty()) {
+					return b;
+				}
+				if (b.isEmpty()) {
+					return a;
+				}
+				if (comparator.compare(a.get(0), b.get(0)) == 0) {
+					a.addAll(b);
+					return a;
+				}
+				if (comparator.compare(a.get(0), b.get(0)) < 0) {
+					return a;
+				}
+				return b;
+			}
+		};
 	}
 
 	@Override
 	public Function<List<T>, List<T>> finisher() {
-		throw new DayException("Not implemented yet!"); // TODO
+		return Collections::unmodifiableList;
 	}
 
 	@Override
 	public Set<Characteristics> characteristics() {
-		throw new DayException("Not implemented yet!"); // TODO
+		return EnumSet.of(Characteristics.CONCURRENT, Characteristics.UNORDERED);
 	}
 }
