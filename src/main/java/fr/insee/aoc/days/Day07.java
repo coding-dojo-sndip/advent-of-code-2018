c5ed4749c3d4d01e0fb64193587c3d8b329f9d13
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-02-28 13:06:10 +0100

    Classes et input Day07

---
+++ src/main/java/fr/insee/aoc/days/Day07.java
@@ -0,0 +1,10 @@
+package fr.insee.aoc.days;
+
+import static fr.insee.aoc.utils.Days.*;
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
+import static fr.insee.aoc.utils.Collectors.*;
+
+public class Day07 implements Day {
+
+}
---
+++ src/test/java/fr/insee/aoc/days/Day07Test.java
@@ -0,0 +1,33 @@
+package fr.insee.aoc.days;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day07Test {
+    
+	private Day day = new Day07();
+	
+	@Test
+	public void case1_0() {
+		Assert.assertEquals("17", day.part1("src/test/resources/07-0.txt"));
+	}
+	
+	@Test
+	public void case2_0() {
+		Assert.assertEquals("16", day.part2("src/test/resources/07-0.txt", 2));
+	}
+	
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/07.txt");
+		System.out.println(String.format("%s.1: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("", answer);
+	}
+	
+	@Test
+	public void part2() {
+		String answer = day.part2("src/main/resources/07.txt", 5);
+		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("", answer);
+	}
+}
