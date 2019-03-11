bf0bb954e83ab110fda1d2cd773e2775dda3a843
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-03-11 09:38:57 +0100

    Mise en place day08

---
+++ src/test/java/fr/insee/aoc/days/Day08Test.java
@@ -0,0 +1,33 @@
+package fr.insee.aoc.days;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day08Test {
+    
+	private Day day = new Day08();
+	
+	@Test
+	public void case1_0() {
+		Assert.assertEquals("138", day.part1("src/test/resources/08-0.txt"));
+	}
+	
+	@Test
+	public void case2_0() {
+		Assert.assertEquals("66", day.part2("src/test/resources/08-0.txt"));
+	}
+	
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/08-rw.txt");
+		System.out.println(String.format("%s.1: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("36627", answer);
+	}
+	
+	@Test
+	public void part2() {
+		String answer = day.part2("src/main/resources/08-rw.txt");
+		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
+		Assert.assertEquals("16695", answer);
+	}
+}
