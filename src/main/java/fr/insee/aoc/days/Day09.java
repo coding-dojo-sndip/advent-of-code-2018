5d8d26782697d10b17f7e50a43cea0e07ffe249f
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-04-26 07:35:11 +0200

    Noms de variables

--- src/main/java/fr/insee/aoc/days/Day09.java
+++ src/main/java/fr/insee/aoc/days/Day09.java
@@ -15,7 +15,7 @@ public class Day09 implements Day {
 	public String part1(String input, Object... params) {
 		int numberOfPlayers = (int) params[0];
 		int marbleMaxValue = (int) params[1];
-		long maxScore = play(numberOfPlayers, marbleMaxValue);
+		long maxScore = maxScore(numberOfPlayers, marbleMaxValue);
 		return String.valueOf(maxScore);
 	}
 	
@@ -24,56 +24,55 @@ public class Day09 implements Day {
 		return part1(input, params[0], (int) params[1] * 100);
 	}
 
-	private long play(int numberOfPlayers, int marbleMaxValue) {
+	private long maxScore(int numberOfPlayers, int marbleMaxValue) {
 		Map<Integer, Long> scores = IntStream
 			.rangeClosed(1, numberOfPlayers)
 			.boxed()
 			.collect(toMap(identity(), t -> 0L));
 
 		List<Integer> marbles = new LinkedList<>();
-		ListIterator<Integer> listIterator = marbles.listIterator();
+		ListIterator<Integer> iterator = marbles.listIterator();
 
 		int currentPlayer = 0;
-		listIterator.add(0);
+		iterator.add(0);
 
 		for (int marble = 1; marble <= marbleMaxValue; marble++) {
 			currentPlayer = (currentPlayer + 1) % numberOfPlayers;
 
 			if (marble % 23 != 0) {
-				listIterator = moveNext(marbles, listIterator);
-				listIterator.add(marble);
+				iterator = moveNext(marbles, iterator);
+				iterator.add(marble);
 			} else {
 				for (int j = 0; j <= 7; j++) {
-					listIterator = movePrevious(marbles, listIterator);
+					iterator = movePrevious(marbles, iterator);
 				}
-				listIterator.next();
-				int points = listIterator.previous() + marble;
-				scores.computeIfPresent(currentPlayer, (joueur, score) -> score + points);
-				listIterator.remove();
-				moveNext(marbles, listIterator);
+				iterator.next();
+				int points = iterator.previous() + marble;
+				scores.computeIfPresent(currentPlayer, (player, score) -> score + points);
+				iterator.remove();
+				moveNext(marbles, iterator);
 			}
 		}
-		long maxScore = scores.values()
+		return scores.values()
 			.stream()
 			.mapToLong(Long::longValue)
 			.max()
 			.orElse(-1);
-		return maxScore;
 	}
 	
-	private static ListIterator<Integer> movePrevious(List<Integer> marbles, ListIterator<Integer> listIterator) {
-		if (!listIterator.hasPrevious()) {
-			listIterator = marbles.listIterator(marbles.size());
+	private static ListIterator<Integer> movePrevious(List<Integer> marbles, ListIterator<Integer> iterator) {
+		if (!iterator.hasPrevious()) {
+			iterator = marbles.listIterator(marbles.size());
 		}
-		listIterator.previous();
-		return listIterator;
+		iterator.previous();
+		return iterator;
 	}
 
-	private static ListIterator<Integer> moveNext(List<Integer> marbles, ListIterator<Integer> listIterator) {
-		if (!listIterator.hasNext()) {
-			listIterator = marbles.listIterator();
+	private static ListIterator<Integer> moveNext(List<Integer> marbles, ListIterator<Integer> iterator) {
+		if (!iterator.hasNext()) {
+			iterator = marbles.listIterator();
 		}
-		listIterator.next();
-		return listIterator;
+		iterator.next();
+		return iterator;
 	}
 }
