3355aae25dff37b4ca03cb5f6ada5cbececc78c0
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-05-17 07:52:41 +0200

    Ajout test unitaire partie 2

--- src/test/java/fr/insee/aoc/days/Day10Test.java
+++ src/test/java/fr/insee/aoc/days/Day10Test.java
@@ -7,18 +7,23 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Day10Test {
-    
+
 	private Day day = new Day10();
 
 	private static String textFromFile(String answerFile) {
-	    return streamOfLines(answerFile).collect(joining(String.format("%n")));
-    }
+		return streamOfLines(answerFile).collect(joining(String.format("%n")));
+	}
 
 	@Test
 	public void case1_0() {
 		Assert.assertEquals(textFromFile("src/test/resources/10-0-s.txt"), day.part1("src/test/resources/10-0.txt"));
 	}
-	
+
+	@Test
+	public void case2_0() {
+		Assert.assertEquals("3", day.part2("src/test/resources/10-0.txt"));
+	}
+
 	@Test
 	public void part1() {
 		String answer = day.part1("src/main/resources/10.txt");
