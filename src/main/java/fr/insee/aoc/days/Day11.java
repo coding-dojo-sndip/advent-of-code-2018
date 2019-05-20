e79bcb40174b397e39b334e6303eb953918f121a
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-05-20 09:58:52 +0200

    Début day11

--- src/main/java/fr/insee/aoc/days/Day11.java
+++ src/main/java/fr/insee/aoc/days/Day11.java
@@ -1,9 +1,35 @@
 package fr.insee.aoc.days;
 
 import static java.util.stream.Collectors.*;
+
+import static java.util.stream.IntStream.*;
+
+import fr.insee.aoc.utils.Point;
+
 import static fr.insee.aoc.utils.Days.*;
 import static fr.insee.aoc.utils.Frame.*;
 
 public class Day11 implements Day {
 
+	@Override
+	public String part1(String input, Object... params) {
+		int gridSerial = Integer.valueOf(readLine(input));
+		int[][] grid = grid(gridSerial);
+		return "0";
+	}
+
+	private int[][] grid(int gridSerial) {
+		int[][] grid = new int[300][300];
+		range(0, 300).forEach(x -> {
+			range(0, 300).forEach(y -> {
+				grid[x][y] = powerLevel(x + 1, y + 1, gridSerial);
+			});
+		});
+		return grid;
+	}
+	
+	private int powerLevel(int x, int y, int gridSerial) {
+		int rackId = x + 10;
+		return (((rackId * y + gridSerial) * rackId / 100) % 10) - 5;
+	}
 }
