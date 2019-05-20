41f672a9613ae2a71e4fabe74f238d4749db1f4e
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-05-20 09:45:34 +0200

    Mise en place du Day11 (mauvaises données) : 9005 en vrai

---
+++ src/main/java/fr/insee/aoc/days/Day11.java
@@ -0,0 +1,9 @@
+package fr.insee.aoc.days;
+
+import static java.util.stream.Collectors.*;
+import static fr.insee.aoc.utils.Days.*;
+import static fr.insee.aoc.utils.Frame.*;
+
+public class Day11 implements Day {
+
+}
