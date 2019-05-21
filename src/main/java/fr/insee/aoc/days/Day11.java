c2c09b65e033e531a1a6c70060ff642606d28fb9
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-05-21 09:05:54 +0200

    Day1, partie 1.

--- src/main/java/fr/insee/aoc/days/Day11.java
+++ src/main/java/fr/insee/aoc/days/Day11.java
@@ -1,13 +1,9 @@
 package fr.insee.aoc.days;
 
-import static java.util.stream.Collectors.*;
-
-import static java.util.stream.IntStream.*;
+import static fr.insee.aoc.utils.Days.readLine;
+import static java.util.stream.IntStream.range;
 
 import fr.insee.aoc.utils.Point;
-
-import static fr.insee.aoc.utils.Days.*;
-import static fr.insee.aoc.utils.Frame.*;
 
 public class Day11 implements Day {
 
@@ -15,7 +11,18 @@ public class Day11 implements Day {
 	public String part1(String input, Object... params) {
 		int gridSerial = Integer.valueOf(readLine(input));
 		int[][] grid = grid(gridSerial);
-		return "0";
+		int largestTotalPower = Integer.MIN_VALUE;
+		Point point = null;
+		for(int x = 0; x < 297; x ++) {
+			for(int y = 0; y < 297; y ++) {
+				int totalPower = totalPower(x, y, grid);
+				if(totalPower > largestTotalPower) {
+					point = Point.of(x + 1, y + 1);
+					largestTotalPower = totalPower;
+				}
+			}
+		}
+		return String.format("%d,%d", point.getX(), point.getY());
 	}
 
 	private int[][] grid(int gridSerial) {
@@ -31,5 +38,15 @@ public class Day11 implements Day {
 	private int powerLevel(int x, int y, int gridSerial) {
 		int rackId = x + 10;
 		return (((rackId * y + gridSerial) * rackId / 100) % 10) - 5;
+	}
+	
+	private int totalPower(int x, int y, int[][] grid) {
+		int totalPower = 0;
+		for(int i = x; i < x + 3; i ++) {
+			for(int j = y; j < y + 3; j ++) {
+				totalPower += grid[i][j];
+			}
+		}
+		return totalPower;
 	}
 }
