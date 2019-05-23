7d16334ff7feb6a7a379935276e3ec9e26a7f122
coding-dojo-sndip <coding.dojo.sndip@gmail.com>
2019-05-23 16:10:09 +0200

    Jour 11, partie 1

--- src/main/java/fr/insee/aoc/days/Day11.java
+++ src/main/java/fr/insee/aoc/days/Day11.java
@@ -1,9 +1,55 @@
 package fr.insee.aoc.days;
 
 import static java.util.stream.Collectors.*;
+
+import fr.insee.aoc.utils.Point;
+
 import static fr.insee.aoc.utils.Days.*;
 import static fr.insee.aoc.utils.Frame.*;
 
 public class Day11 implements Day {
+
+	@Override
+	public String part1(String input, Object... params) {
+		int gridSerial = Integer.valueOf(readLine(input));
+		Point point = pointWithMaxTotalPower(gridSerial);
+		return String.format("%d,%d", point.getX(), point.getY());
+	}
+
+	static int powerLevel(int x, int y, int gridSerial) {
+		int rackId = x + 10;
+		return ((((rackId * y + gridSerial) * rackId) % 1000) / 100) - 5;
+	}
+
+	static int[][] createGrid(int gridSerial) {
+		int[][] grid = new int[301][301];
+		for (int x = 1; x <= 300; x++) {
+			for (int y = 1; y <= 300; y++) {
+				grid[x][y] = powerLevel(x, y, gridSerial);
+			}
+		}
+		return grid;
+	}
+
+	static Point pointWithMaxTotalPower(int gridSerial) {
+		int[][] grid = createGrid(gridSerial);
+		int maxTotalPower = Integer.MIN_VALUE;
+		Point point = null;
+		for (int x = 1; x <= 298; x++) {
+			for (int y = 1; y <= 298; y++) {
+				int totalPower = 0;
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+						totalPower = totalPower + grid[x + i][y + j];
+					}
+				}
+				if (totalPower > maxTotalPower) {
+					maxTotalPower = totalPower;
+					point = Point.of(x, y);
+				}
+			}
+		}
+		return point;
+	}
 
 }
--- src/test/java/fr/insee/aoc/days/Day11Test.java
+++ src/test/java/fr/insee/aoc/days/Day11Test.java
@@ -2,6 +2,7 @@ package fr.insee.aoc.days;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static fr.insee.aoc.days.Day11.*;
 
 public class Day11Test {
 
@@ -9,12 +10,12 @@ public class Day11Test {
 
 	@Test
 	public void case1_0() {
-		Assert.assertEquals("33,45", day.part1("src/test/resources/11-0.txt"));
+		Assert.assertEquals("21,61", day.part1("src/test/resources/11-0.txt"));
 	}
 	
 	@Test
 	public void case1_1() {
-		Assert.assertEquals("21,61", day.part1("src/test/resources/11-1.txt"));
+		Assert.assertEquals("33,45", day.part1("src/test/resources/11-1.txt"));
 	}
 
 	@Test
@@ -31,7 +32,7 @@ public class Day11Test {
 	public void part1() {
 		String answer = day.part1("src/main/resources/11.txt");
 		System.out.println(String.format("%s.1:%n%s", day.getClass().getSimpleName(), answer));
-		Assert.assertEquals("20,83", answer);
+		Assert.assertEquals("20,32", answer);
 	}
 
 	@Test
@@ -39,5 +40,11 @@ public class Day11Test {
 		String answer = day.part2("src/main/resources/11.txt");
 		System.out.println(String.format("%s.2: %s", day.getClass().getSimpleName(), answer));
 		Assert.assertEquals("237,281,10", answer);
+	}
+	
+	@Test
+	public void testPowerLevel() {
+		int x = 3, y = 5, gridSerial = 8;
+		Assert.assertEquals(4, powerLevel(x, y, gridSerial));
 	}
 }
